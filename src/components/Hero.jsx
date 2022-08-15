39a9a0dbf759fd84fbaaa6ab0fd1588a4986f7e5
orlando <orlando.nabil@gmail.com>
2022-08-15 09:46:14 -0500

    finish

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
-import InputLocation from './InputLocation'
+import React from 'react';
+import InputLocation from './InputLocation';
 
 const Hero = ({ handleChange, handleLocation }) => {
   return (
     <div className="hero  bg-base-200 p-5">
-      <div className="hero-content flex lg:flex-row place-content-center">
-        <img src="src/assets/images/rickmority.jpg" className=" w-40 rounded-lg shadow-2xl" />
-        <div className='flex flex-col text-center place-content-center p-5'>
-          <h1 className="text-5xl mb-5 font-bold dark:text-white">Rick and Morty</h1>
-          <p className="py-6 max-w-md dark:text-white ">Rick and Morty api consumption, type a number between 1 - 126 to see all possible universes.</p>
-          <InputLocation handleLocation={handleLocation} handleChange={handleChange} />
+      <div className="hero-content flex place-content-center lg:flex-row">
+        <img
+          src="https://i.pinimg.com/564x/8c/de/a5/8cdea5a04c79cf3098f19752e5bcf0d8.jpg"
+          className=" w-40 rounded-lg shadow-2xl"
+        />
+        <div className="flex flex-col place-content-center p-5 text-center">
+          <h1 className="mb-5 text-5xl font-bold dark:text-white">
+            Rick and Morty
+          </h1>
+          <p className="max-w-md py-6 dark:text-white ">
+            Rick and Morty api consumption, type a number between 1 - 126 to see
+            all possible universes.
+          </p>
+          <InputLocation
+            handleLocation={handleLocation}
+            handleChange={handleChange}
+          />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
