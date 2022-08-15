59e31cd3247486ebd45fe0de911c8017d38a93da
orlando <orlando.nabil@gmail.com>
2022-08-15 09:37:37 -0500

    finish

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = ({ handleChange, handleLocation }) => {
   return (
     <div className="hero  bg-base-200 p-5">
       <div className="hero-content flex lg:flex-row place-content-center">
-        <img src="src/assets/images/rickmority.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+        <img src="src/assets/images/rickmority.jpg" className=" w-40 rounded-lg shadow-2xl" />
         <div className='flex flex-col text-center place-content-center p-5'>
           <h1 className="text-5xl mb-5 font-bold dark:text-white">Rick and Morty</h1>
           <p className="py-6 max-w-md dark:text-white ">Rick and Morty api consumption, type a number between 1 - 126 to see all possible universes.</p>
--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 const Location = ({ Data }) => {
   return (
-    <div className='w-full flex place-content-center mb-5'>
-      <div class="p-6 w-1/2 text-center text-black bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 dark:text-white">
-        <h3 className='m-3 font-bold'>{Data.name}</h3>
-        <div className=' flex justify-around ' >
-          <p> <strong>Type:</strong> {Data.type} </p>
-          <p> <strong>Dimension:</strong> {Data.dimension}</p>
-          <p> <strong>population:</strong> {Data.residents.length}</p>
+    <div className="mb-5 flex w-full place-content-center">
+      <div class="w-1/2 rounded-lg border border-gray-200 bg-white p-6 text-center text-black shadow-md dark:border-gray-700 dark:bg-gray-800 dark:text-white">
+        <h3 className="m-3 font-bold">{Data.name}</h3>
+        <div className=" flex justify-around ">
+          <p>
+            {' '}
+            <strong>Type:</strong> {Data.type}{' '}
+          </p>
+          <p>
+            {' '}
+            <strong>Dimension:</strong> {Data.dimension}
+          </p>
+          <p>
+            {' '}
+            <strong>population:</strong> {Data.residents.length}
+          </p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Location
+export default Location;
